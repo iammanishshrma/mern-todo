2008b97dbbe777962b46f291b9923b989ccf4341
Manish Sharma <manish.sharma@sparxitsolutions.com>
2023-02-08 19:51:07 +0530

    add todo form added

--- frontend/src/pages/AddTodo.jsx
+++ frontend/src/pages/AddTodo.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const AddTodo = () => {
-    return <div>AddTodo</div>;
+    const submitHandler = (event) => {
+        event.preventDefault();
+    };
+    return (
+        <div>
+            <h1>Add todo</h1>
+            <form onSubmit={submitHandler}>
+                <div>
+                    <label htmlFor="title">Title</label>
+                    <input type="text" id="title" />
+                </div>
+                <div>
+                    <label htmlFor="description">Description</label>
+                    <input type="text" id="description" />
+                </div>
+                <div>
+                    <button type="submit">Add todo</button>
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default AddTodo;
